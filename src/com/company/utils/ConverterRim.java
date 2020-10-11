58b7451b963e51e9ab1540b6a8ccc4472db4255d
ZarDavid <amdzdvd@gmail.com>
2020-10-12 01:51:01 +0600

    Поправил замечание ментора. Умножение римских чисел.

--- src/com/company/utils/ConverterRim.java
+++ src/com/company/utils/ConverterRim.java
@@ -12,12 +12,12 @@ public class ConverterRim {
         if(arab == 1 || arab == 5 || arab == 10 || arab == 50 || arab == 100){
             rez += converterArab(arab);
         }else{
-            rez  += convert(0, rez, arab);
+            rez  += convert(rez, arab);
         }
         return rez.trim();
     }
 
-    private String convert(int border, String rez, int arab){
+    private String convert(String rez, int arab){
 
         if(arab <= 3){
             for(int i = 0; i < arab; i++){
@@ -31,27 +31,27 @@ public class ConverterRim {
             int numOne =  arab - 5;
 
             if(numOne >= 1){
-                rez = converterArab(5) + convert(0, rez, numOne);
+                rez = converterArab(5) + convert(rez, numOne);
             }else{
-                rez = convert(0, rez, Math.abs(numOne)) + converterArab(5);
+                rez = convert(rez, Math.abs(numOne)) + converterArab(5);
             }
         }
 
         if(arab >= 9 && arab < 40){
             int numOne = arab - 10;
             if(numOne >= 1){
-                rez = converterArab(10) + convert(0, rez, numOne);
+                rez = converterArab(10) + convert(rez, numOne);
             } else{
-                rez = convert(0, rez, Math.abs(numOne)) + converterArab(10);
+                rez = convert(rez, Math.abs(numOne)) + converterArab(10);
             }
         }
 
         if(arab >= 40 && arab < 90){
             int numOne = arab - 50;
             if(numOne >= 1){
-                rez = converterArab(50) + convert(0, rez, numOne);
+                rez = converterArab(50) + convert( rez, numOne);
             }else{
-                rez =  "X" + converterArab(50) + convert(0, rez, Math.abs(numOne + 10));
+                rez =  "X" + converterArab(50) + convert(rez, Math.abs(numOne + 10));
             }
         }
 
@@ -59,10 +59,10 @@ public class ConverterRim {
             //System.out.println(arab);
             int numOne = arab - 90;
             if(numOne >= 1){
-                rez = "X" + converterArab(100) + convert(0, rez, numOne);
+                rez = "X" + converterArab(100) + convert(rez, numOne);
             }
             else{
-                rez = "X" + convert(0, rez, Math.abs(numOne)) + converterArab(100);
+                rez = "X" + convert(rez, Math.abs(numOne)) + converterArab(100);
             }
         }
         return rez;
